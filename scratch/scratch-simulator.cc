f20d51b0b86b2c0e3cc39a6f9e954de4aa7cb620
Guilherme David Branco <gdbranco@gmail.com>
2018-10-16 22:03:58 -0300

    UDP server and clients working with remote UDP server connected to EPC. TODO: Change it to HTTP server and clients

--- scratch/scratch-simulator.cc
+++ scratch/scratch-simulator.cc
@@ -6,9 +6,8 @@
 #include "ns3/config-store.h"
 #include "ns3/buildings-helper.h"
 #include "ns3/netanim-module.h"
-
-//#include "ns3/gtk-config-store.h"
-
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
 using namespace std;
 using namespace ns3;
 
@@ -18,22 +17,15 @@ int main (int argc, char *argv[])
     SeedManager::SetRun(42);
     //whether to use carrier aggregation
     bool useCa = false;
+    double simTime = 30;
+    double interPacketInterval = 25;
+    uint32_t nEnb = 1;
     uint32_t nUE = 2;
-    double simTime = 1.5;
 
     CommandLine cmd;
     cmd.AddValue("useCa", "Whether to use carrier aggregation.", useCa);
     cmd.AddValue("nUe", "Number of UEs", nUE);
     cmd.AddValue("simTime", "Total duration of the simulation (in seconds)", simTime);
-
-    // to save a template default attribute file run it like this:
-    // ./waf --command-template="%s --ns3::ConfigStore::Filename=input-defaults.txt --ns3::ConfigStore::Mode=Save --ns3::ConfigStore::FileFormat=RawText" --run src/lte/examples/lena-first-sim
-    //
-    // to load a previously created default attribute file
-    // ./waf --command-template="%s --ns3::ConfigStore::Filename=input-defaults.txt --ns3::ConfigStore::Mode=Load --ns3::ConfigStore::FileFormat=RawText" --run src/lte/examples/lena-first-sim
-
-    ConfigStore inputConfig;
-    inputConfig.ConfigureDefaults ();
 
     // Parse again so you can override default values from the command line
     cmd.Parse (argc, argv);
@@ -49,7 +41,6 @@ int main (int argc, char *argv[])
     double roomLength = 50000;
     double nodeHeight = 1.5;
     double bsHeight = 5;
-    uint32_t nEnb = 1;
 
 
     // HTTP Stuff
@@ -57,12 +48,34 @@ int main (int argc, char *argv[])
 
     // Create HTTP Client
 
+
+    // Container for all nodes
+    NodeContainer allNodes;
     // LTE HELPER constructor
     Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+    Ptr<PointToPointEpcHelper> epcHelper = CreateObject<PointToPointEpcHelper>();
+    lteHelper->SetEpcHelper(epcHelper);
+    /************* ANTENNA SETUP ************/
     lteHelper->SetAttribute("PathlossModel", StringValue("ns3::FriisPropagationLossModel"));
+    lteHelper->SetPathlossModelAttribute("SystemLoss", DoubleValue(0.0));
+    lteHelper->SetEnbAntennaModelType("ns3::CosineAntennaModel");
+    lteHelper->SetEnbAntennaModelAttribute("Orientation", DoubleValue(0));
+    lteHelper->SetEnbAntennaModelAttribute("Beamwidth", DoubleValue(35));
+    lteHelper->SetEnbAntennaModelAttribute("MaxGain", DoubleValue(400));
+    // Default scheduler is PF, uncomment to use RR
+    //lteHelper->SetSchedulerType ("ns3::RrFfMacScheduler");
 
+    // PGW constructor
+    Ptr<Node> pgw = epcHelper->GetPgwNode();
+    
+    allNodes.Add(pgw);
     // Uncomment to enable logging
 //  lteHelper->EnableLogComponents ();
+
+    // Remote Host
+    NodeContainer remoteHostContainer;
+    remoteHostContainer.Create(1);
+    allNodes.Add(remoteHostContainer);
 
     // Create Nodes: eNodeB and UE
     NodeContainer enbNodes;
@@ -70,62 +83,95 @@ int main (int argc, char *argv[])
     // As there is only one floor just create everything within node container
     enbNodes.Create (nEnb);
     ueNodes.Create (nUE);
+    allNodes.Add(enbNodes);
+    allNodes.Add(ueNodes);
 
-    // Install Mobility Model
-    std::vector<Vector> enBPosition;
-    std::vector<Vector> UEPosition;
-    Ptr<ListPositionAllocator> bsPositionAlloc = CreateObject<ListPositionAllocator> ();
-    Ptr<ListPositionAllocator> uePositionAlloc = CreateObject<ListPositionAllocator> ();
-    //EnB position
-    Vector v(roomLength/2, roomLength/2, bsHeight);
-    bsPositionAlloc->Add(v);
-    enBPosition.push_back(v);
-    MobilityHelper mobility;
-    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-    mobility.SetPositionAllocator(bsPositionAlloc);
-    mobility.Install (enbNodes);
-    cout << "enB Pos: ";
-    cout << "(" << enBPosition.at(0).x << "," << enBPosition.at(0).y << ")" << endl;
-    // UE position
-    for(uint32_t j=0; j<nUE;j++){
+    /***************MOBILITY SETUP**************/
+    Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator>();
+    positionAlloc->Add(Vector(10, 0, 0)); // 0 - PGW
+    positionAlloc->Add(Vector(0, 0, 0)); // 1 - Internet
+    positionAlloc->Add(Vector(12500, 12500, bsHeight)); // 2 - eNB
+    for (uint32_t j=0;j<nUE;j++){
         Ptr<UniformRandomVariable> posX = CreateObject<UniformRandomVariable> ();
         Ptr<UniformRandomVariable> posY = CreateObject<UniformRandomVariable> ();
-        v = Vector(posX->GetValue()*roomLength, posY->GetValue()*roomLength, nodeHeight);
-        cout << "UE " << j << " POS ";
-        cout << "(" << v.x << "," << v.y << ")" << endl;
-        uePositionAlloc->Add(v);
-        UEPosition.push_back(v);
+        positionAlloc->Add(Vector(posX->GetValue()*roomLength, posY->GetValue()*roomLength, nodeHeight));
     }
+    MobilityHelper mobility;
     mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-    mobility.SetPositionAllocator(uePositionAlloc);
-    mobility.Install(ueNodes);
+    mobility.SetPositionAllocator(positionAlloc);
+    mobility.Install (allNodes);
 
+    /*****************IP STACK SETUP**************/
+    Ptr<Node> remoteHost = remoteHostContainer.Get(0);
+    InternetStackHelper internet;
+    internet.Install(remoteHostContainer);
+
+    // Connect internet to PGW
+    PointToPointHelper p2p;
+    p2p.SetDeviceAttribute("DataRate", DataRateValue(DataRate ("100Gb/s")));
+    p2p.SetDeviceAttribute("Mtu", UintegerValue(1500));
+    p2p.SetChannelAttribute("Delay", TimeValue(Seconds(0.010)));
+    NetDeviceContainer internetDevices = p2p.Install(pgw, remoteHost);
+
+    // Put IP in nodes
+    Ipv4AddressHelper ipv4h;
+    ipv4h.SetBase("1.0.0.0","255.0.0.0");
+    Ipv4InterfaceContainer internetIPfaces = ipv4h.Assign(internetDevices);
+
+    Ipv4StaticRoutingHelper ipv4RoutingHelper;
+    Ptr<Ipv4StaticRouting> remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting(remoteHost->GetObject<Ipv4>());
+    remoteHostStaticRouting->AddNetworkRouteTo(Ipv4Address("7.0.0.0"), Ipv4Mask("255.0.0.0"),1);
+
+    /*******************CREATE LTE DEVICES**************/
     // Create Devices and install them in the Nodes (eNB and UE)
-    NetDeviceContainer enbDevs;
-    NetDeviceContainer ueDevs;
-    // Default scheduler is PF, uncomment to use RR
-    //lteHelper->SetSchedulerType ("ns3::RrFfMacScheduler");
+    NetDeviceContainer enbDevs = lteHelper->InstallEnbDevice(enbNodes);
+    NetDeviceContainer ueDevs = lteHelper->InstallUeDevice (ueNodes);
+    internet.Install(ueNodes);
+    Ipv4InterfaceContainer ueIPface = epcHelper->AssignUeIpv4Address(NetDeviceContainer(ueDevs));
 
-    enbDevs = lteHelper->InstallEnbDevice (enbNodes);
-    ueDevs = lteHelper->InstallUeDevice (ueNodes);
-
+    for(uint32_t j = 0;j<ueNodes.GetN();++j){
+        Ptr<Node> ueNode = ueNodes.Get(j);
+        Ptr<Ipv4StaticRouting> ueStaticRouting = ipv4RoutingHelper.GetStaticRouting(ueNode->GetObject<Ipv4>());
+        ueStaticRouting->SetDefaultRoute(epcHelper->GetUeDefaultGatewayAddress(), 1);
+    }
     // Attach a UE devices to a eNB
     lteHelper->Attach (ueDevs, enbDevs.Get (0));
 
     // Activate a data radio bearer
-    enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
-    EpsBearer bearer (q);
-    lteHelper->ActivateDataRadioBearer (ueDevs, bearer);
+    // enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
+    // EpsBearer bearer (q);
+    // lteHelper->ActivateDataRadioBearer (ueDevs, bearer);
+
+
+    /************APPLICATION SETUP********************/
+    uint16_t serverPort = 9;
+
+    UdpEchoServerHelper echoServer(serverPort);
+    ApplicationContainer serverApps = echoServer.Install(remoteHost);
+
+    serverApps.Start(Seconds(0.5));
+
+    UdpEchoClientHelper echoClient(remoteHost->GetObject<Ipv4>()->GetAddress(1,0).GetLocal(),serverPort);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(10000));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientApps;
+    clientApps.Add(echoClient.Install(ueNodes));
+    clientApps.Start(Seconds(0.7));
+
+    /*********ENABLE TRACES****************/
 
     // RUN SIM
-    Simulator::Stop (Seconds (simTime));
     lteHelper->EnableTraces ();
 
     AnimationInterface anim("animation.xml");
     anim.SetMobilityPollInterval(Seconds(.25));
+    anim.SetMaxPktsPerTraceFile(0xFFFFFFFF);
     anim.EnablePacketMetadata(true);
 
     // Start simulation and frees up when done
+    Simulator::Stop (Seconds (simTime));
     Simulator::Run ();
     Simulator::Destroy ();
     return 0;
